98d39daa9a0262358a8d58064aba334bb04b16a9
jamily <jamilyfernandes71@gmail.com>
2022-05-12 23:10:04 -0300

    projeto

---
+++ projetoavancar/src/main/java/com/avancar/projetoavancar/ProjetoavancarApplication.java
@@ -0,0 +1,13 @@
+package com.avancar.projetoavancar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoavancarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoavancarApplication.class, args);
+	}
+
+}
